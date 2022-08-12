3ed000fefdee5dbfb34f4979d950873539f796ac
roopaish <54392112+Roopaish@users.noreply.github.com>
2022-08-12 22:38:41 +0545

    changes cors

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT ?? 3000;
 const server = http.createServer(async (req, res) => {
   await handleRequests(req, res);
 });
+// const corsOrigin = 'http://127.0.0.1:5500';
+const corsOrigin = "https://roopaish.github.io/";
 
 const handleRequests = async (req, res) => {
   // Daraz api requests
@@ -15,19 +17,25 @@ const handleRequests = async (req, res) => {
 
       res.writeHead(200, {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "http://127.0.0.1:5500",
+        "Access-Control-Allow-Origin": `${corsOrigin}`,
       });
       res.write(JSON.stringify(data));
       res.end();
     } catch (e) {
-      res.writeHead(404, { "Content-Type": "application/json" });
+      res.writeHead(404, {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": `${corsOrigin}`,
+      });
       res.end(JSON.stringify({ message: "Something went wrong!" }));
     }
   }
 
   // No route response
   else {
-    res.writeHead(404, { "Content-Type": "application/json" });
+    res.writeHead(404, {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": `${corsOrigin}`,
+    });
     res.end(JSON.stringify({ message: "Route not found" }));
   }
 };
