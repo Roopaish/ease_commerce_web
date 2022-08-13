3b689e96d2b5db0fa60e919213cbf3c58c542efc
roopaish <54392112+Roopaish@users.noreply.github.com>
2022-08-13 08:49:00 +0545

    product modal viewer

--- app.js
+++ app.js
@@ -7,25 +7,39 @@ const darazProductListEl = document.querySelector(
 );
 const searchFormEl = document.querySelector(".search-form");
 const searchInputEl = document.querySelector(".search-box input");
+const modalEl = document.querySelector(".modal");
+let productEls = document.querySelectorAll(".product");
+let productsData = [];
 // const pageTitleEl = document.querySelector(".page-title");
 
-generateProductListItem = (name, imageUrl, url, price) => {
+const generateProductListItem = (item) => {
+  const {
+    name,
+    nid,
+    image,
+    productUrl,
+    priceShow,
+    description,
+    sellerName,
+    price,
+  } = item;
+  // description = <String>[]
   return `
-    <div class="product" data-url=${url}>
+    <div class="product" data-id=${nid}>
       <div class="product-image">
-        <img src=${imageUrl} alt=${name} />
+        <img src=${image} alt=${name} />
       </div>
       <div class="product-details">
         <h4 class="product-name">${name}</h4>
         <div class="product-price">
-          <span>${price}</span>
+          <span>${priceShow}</span>
         </div>
       </div>
     </div>
   `;
 };
 
-generateNotFoundUI = () => {
+const generateNotFoundUI = () => {
   return `
   <div class='empty-list'>
     <figure>
@@ -37,7 +51,7 @@ generateNotFoundUI = () => {
   `;
 };
 
-generateLoadingUI = () => {
+const generateLoadingUI = () => {
   return `
   <div class="loader">
     <span></span>
@@ -51,6 +65,56 @@ generateLoadingUI = () => {
   `;
 };
 
+// Modal Actions
+const openModal = (productId) => {
+  modalEl.classList.remove("hide");
+  modalEl.classList.add("show");
+  const item = productsData.find((e) => e.nid == productId);
+  const { name, image, priceShow, sellerName, description, productUrl } = item;
+
+  let desEl = ``;
+  description.map((des) => {
+    desEl += `
+  <li>${des}</li>
+  `;
+  });
+
+  modalEl.innerHTML = `
+  <div class="close-button">
+      <i class="lni lni-close"></i>
+    </div>
+    <div class="container">
+      <h3>${name}</h3>
+      <div class="product-info-grid">
+        <figure>
+          <img src=${image} alt=${name} />
+        </figure>
+        <div class="product-info">
+          <div>
+            <span> Price:</span> ${priceShow}
+          </div>
+          <div>
+            <span>SellerName:</span> ${sellerName}
+          </div>
+          <p>
+            ${desEl}
+          </p>
+          <p>
+            <a href=${productUrl} target="_blank">Go to Official site</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  `;
+
+  const modalCloseButtonEl = document.querySelector(".close-button");
+
+  modalCloseButtonEl.addEventListener("click", () => {
+    modalEl.classList.add("hide");
+    modalEl.classList.remove("show");
+  });
+};
+
 const getItemsFromDaraz = async (name) => {
   try {
     // pageTitleEl.innerHTML = `Search Results for <div style='font-style:italic; display:inline; font-weight:600;'>"${name}"</div>`;
@@ -61,24 +125,35 @@ const getItemsFromDaraz = async (name) => {
 
     darazProductListEl.innerHTML = "";
     data.mods.listItems.forEach((item) => {
-      const { name, image, productUrl, priceShow } = item;
-      darazProductListEl.innerHTML += generateProductListItem(
-        name,
-        image,
-        productUrl,
-        priceShow
-      );
+      darazProductListEl.innerHTML += generateProductListItem(item);
     });
 
     darazProductListEl.style.cssText = "overflow-x:scroll;";
-    console.log(data);
+    productsData = data.mods.listItems;
   } catch (e) {
     darazProductListEl.innerHTML = generateNotFoundUI();
   }
+  fetchNewlyAddedProductsEls();
 };
 
 searchFormEl.addEventListener("submit", async (e) => {
   e.preventDefault();
+  searchFormEl.blur();
+  searchInputEl.blur();
   const name = searchInputEl.value;
   await getItemsFromDaraz(name);
 });
+
+addEventListener("DOMContentLoaded", () => {
+  getItemsFromDaraz("laptop");
+});
+
+const fetchNewlyAddedProductsEls = () => {
+  productEls = document.querySelectorAll(".product");
+  productEls.forEach((productEl) => {
+    productEl.addEventListener("click", (e) => {
+      const productId = productEl.getAttribute("data-id");
+      openModal(productId);
+    });
+  });
+};
