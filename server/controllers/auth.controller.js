8f99537cfa1ac3e4b364474f797cb8a1572e4ba6
Rupesh Budhathoki <54392112+Roopaish@users.noreply.github.com>
2022-11-04 13:15:49 +0545

    change same site

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -37,6 +37,8 @@ export const signin = async (req, res, next) => {
     res
       .cookie("access_token", token, {
         httpOnly: true,
+        sameSite: "none",
+        secure: true,
       })
       .status(200)
       .json(others);
