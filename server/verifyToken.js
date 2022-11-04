ca5972427997ac87e4903ba59f604d52895a8c03
Rupesh Budhathoki <54392112+Roopaish@users.noreply.github.com>
2022-11-04 13:20:52 +0545

    token corrected

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -6,7 +6,7 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) return next(createError(401, "You are not authenticated!"));
 
-  jwt.verify(token, process.env.jwt, (err, user) => {
+  jwt.verify(token, process.env.JWT, (err, user) => {
     if (err) return next(createError(403, "Invalid Token!"));
 
     req.user = user; // adding user in request body before passing to other functions
